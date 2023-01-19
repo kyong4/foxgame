8b2412498f7fcc0913ddda571052cf7cae842369
Kevin Yong <euler004@gmail.com>
2023-01-18 20:42:24 -0500

    initial commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+using this site to learn:
+https://www.youtube.com/watch?v=GFO_txvwK_c&ab_channel=freeCodeCamp.org
+
+typed the code from that video above.
+
+source is freecodecamp
---
+++ main.js
@@ -0,0 +1,96 @@
+import {Player } from './player.js';
+import { InputHandler} from './input.js';
+import { Background } from './background.js';
+import { FlyingEnemy, ClimbingEnemy, GroundEnemy} from './enemies.js';
+import { UI } from './UI.js';
+
+window.addEventListener('load', function(){
+const canvas = document.getElementById('canvas1');
+const ctx = canvas.getContext('2d' );
+canvas.width = 900;
+canvas.height = 500;
+
+class Game {
+constructor (width, height) {
+this.width = width;
+this.height = height;
+this.groundMargin = 80;
+this.speed = 0;
+this.maxSpeed = 3;
+this.background = new Background(this);
+this.player = new Player(this);
+this.input = new InputHandler(this);
+this.UI = new UI (this);
+this.enemies = [];
+this.particles=[];
+this.collisions = [];
+this.maxParticles = 200;
+this.enemyTimer=0;
+this.enemyInterval=1000;
+this.debug=true;
+this.score=0;
+this.fontColor= 'black';
+this.time = 0;
+this.maxTime = 30000;
+this.gameOver =false;
+this.lives =5;
+this.player.currentState = this.player.states [0];
+this.player.currentState.enter();
+}
+update (deltaTime) {
+    this.time+=deltaTime;
+    if(this.time>this.maxTime) this.gameOver=true;
+this.background.update();
+this.player.update(this.input.keys, deltaTime);
+// handleEnemies
+if (this.enemyTimer > this.enemyInterval){
+this.addEnemy();
+this.enemyTimer=0;
+} else { //good up to here
+this.time += deltaTime;
+if (this.time > this.maxTime) this.gameOver = true;
+
+
+if (this.enemyTimer >this.enemyInterval) {
+    this.addEnemy();
+    this.enemyTimer=0;
+} else {
+    this.enemyTimer+=deltaTime;
+}
+this.enemies.forEach(enemy=>{
+    enemy.update(deltaTime);
+    if(enemy.markedForDeletion) this.enemies.splice(this.enemies.indexOf(enemy,1))
+});
+}
+}
+draw(context){
+    this.background.draw(context);
+    this.player.draw(context);
+    this.enemies.forEach(enemy=>{
+        enemy.draw(context);
+    });
+} 
+addEnemy(){
+    if(this.speed>0&&Math.random()<0.5) this.enemies.push(new GroundEnemy(this));
+    else if (this.speed>0) this.enemies.push(new ClimbingEnemy(this));
+
+    this.enemies.push(new FlyingEnemy(this));
+    console.log(this.enemies);
+}
+}
+
+
+const game=new Game (canvas.width,canvas.height);
+console.log(game);
+let lastTime=0;
+
+function animate(timestamp) {
+    const deltaTime=timeStamp-lastTime;
+    lastTime=timeStamp;
+    ctx.clearRect(0,0,canvaswidth,canvas.height);
+    game.update(deltaTime);
+    game.draw(ctx);
+   if (!game.gameOver) requestAnimationFrame(animate);
+}
+animate(0);
+});
